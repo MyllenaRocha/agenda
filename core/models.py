f6f83736ee4bdaad47ecf3f92d146a664a01f861
AgnaldoCordeiro <agske13@gmail.com>
2021-11-14 20:22:37 -0300

    Adicionado funções de alteração e exclusão de dados

--- core/models.py
+++ core/models.py
@@ -9,9 +9,16 @@ class Evento(models.Model):
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local_evento = models.TextField(max_length=50, blank=True, null=True)
 
     class Meta:
         db_table = 'evento'
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y=%m-%dT%H:%M')
